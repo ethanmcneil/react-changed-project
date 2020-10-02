8e9da5093678bf83ea07de37cf8c7f6560c3ef9a
blakewood84 <60270366+blakewood84@users.noreply.github.com>
2020-10-02 14:33:56 -0700

    first commit

---
+++ events-application/events-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '96271ba73deafe40b1f40826ad427028'),
+    },
+  },
+});
